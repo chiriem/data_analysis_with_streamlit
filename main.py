25108b921aaba6f7bb3021175b240479839adfa0
chiriem <maoru23@gmail.com>
2025-11-10 11:12:21 +0900

    deploy_test

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -32,31 +31,4 @@ plt.bar(df_jongno.index, df_jongno)
 plt.title("연도별 종로구 cctv 수")
 plt.xlabel("년도")
 plt.ylabel("갯수")
-=======
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-# windows용 한글 폰트 오류 해결
-from matplotlib import font_manager, rc
-font_path = "C:/Windows/Fonts/malgun.ttf"
-font_name = font_manager.FontProperties(fname= font_path).get_name()
-rc("font", family = font_name)
-
-st.title("차트 그리기")
-
-scy_df = pd.read_excel("seoul_cctv_byyear.xlsx")
-
-st.dataframe(scy_df)
-
-df_jongno = scy_df.iloc[0,1:]
-
-# 차트 그리기
-fig = plt.figure()
-plt.bar(df_jongno.index, df_jongno)
-plt.title("연도별 종로구 cctv 수")
-plt.xlabel("년도")
-plt.ylabel("갯수")
->>>>>>> a171c06c708b7651438d53cf0b85cf8867881e37
 st.pyplot(fig)
