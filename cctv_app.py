12bae40b03266ab323b6085a7fb9d89e74ab6916
chiriem <maoru23@gmail.com>
2025-11-11 12:28:03 +0900

    folium complete

--- cctv_app.py
+++ cctv_app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+import folium
+from folium.plugins import MarkerCluster
+from streamlit_folium import folium_static
 
 import os
 import matplotlib.font_manager as fm # 한글 폰트 관련 용도
@@ -23,7 +26,7 @@ def run_cctv_app():
     plt.rc('font', family="Malgun Gothic")
 
     # 메뉴 지정
-    submenu = st.sidebar.selectbox("submenu", ['연도별 CCTV', '시각화', '분석'])
+    submenu = st.sidebar.selectbox("submenu", ['연도별 CCTV', 'CCTV 위치', '분석'])
     if submenu == "submenu":
         st.subheader("submenu")
     elif submenu == "연도별 CCTV":
@@ -64,8 +67,61 @@ def run_cctv_app():
         st.markdown(f"## {a} 대비 {b} 증감률")
         st.markdown(f"### {inc[0].round(1)}% 증가")
 
-    elif submenu == "시각화":
-        st.subheader("시각화")
+    elif submenu == "CCTV 위치":
+        st.subheader("CCTV 위치")
+
+        df_gu = pd.read_excel("seoul_cctv_loc.xlsx")
+
+        gu_list = ["전체"]
+
+        for i in df_gu["자치구"].unique():
+            gu_list.append(i)
+
+        area = st.selectbox("자치구", gu_list)
+
+        if area != "전체":
+            df_gu = df_gu[df_gu["자치구"] == area]
+            focus_on = [df_gu["위도"].mean(), df_gu["경도"].mean()]
+
+            locations = []
+            names = []
+
+            for i in range(len(df_gu)):
+                data = df_gu.iloc[i]
+                locations.append((float(data["위도"]), float(data["경도"])))
+                # names.append(f"{data["CCTV 수량"]}")
+
+            map_gu = folium.Map(location = focus_on, zoom_start = 13)
+            
+            marker_cluster = MarkerCluster(
+                locations= locations,
+                # popups = names,
+                name = area,
+                overlay = True, # 다른 레이어와 겹치기 허용
+                control = True, # 레이어 on/off 박스 표시
+            )
+
+            marker_cluster.add_to(map_gu)
+            folium.LayerControl().add_to(map_gu) # 컨트롤 박스 적용
+
+
+        else:
+            df_gu = pd.read_excel("cctv_count.xlsx")
+            focus_on = [df_gu["위도"].mean(), df_gu["경도"].mean()]
+            map_gu = folium.Map(location = focus_on, zoom_start = 11, zoom_control= False)
+
+            for i in range(len(df_gu)):
+                data = df_gu.iloc[i]
+                iframe = folium.IFrame(f"{data["자치구"]} <br> {data["CCTV 수량"]}개")
+                popup = folium.Popup(iframe, min_width=100, max_width= 300)
+                folium.Marker([data["위도"], data["경도"]], popup=popup).add_to(map_gu)
+        
+
+
+        folium_static(map_gu)
+        
+
+
     elif submenu == "분석":
         st.subheader("분석")
     else: 
